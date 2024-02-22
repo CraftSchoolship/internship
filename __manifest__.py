af76d8c84cc1449d080ea07eea8d3b7308b2255f
Ithar Daouadi <ithardaouadi@gmail.com>
2024-02-22 12:54:31 +0300

    commit1

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name':'Movie Module',
+    'author':'Ithar',
+    'category': 'Tools',
+    'description': """
+    Movie Module test
+    """,
+    'data': [
+        'views/movie.xml',
+        'security/ir.model.access.csv',
+
+     ],
+    
+    'installable': True,
+    'application': True,
+}
