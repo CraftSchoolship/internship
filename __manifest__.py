b292c4fa5d424911ecb8906dc6700b311c7e6792
mor mbathie <mor.mbathie@horizon-tech.tn>
2024-03-18 22:42:13 +0100

    correction du view

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,14 @@
 {
     'name': "Movies",
-    # 'author': "Mor Mbathie",
-    # 'website': "www.odootest.com",
-    # 'summary': "odoo 16 Developement",
-    # 'category': "Tools",
-    # 'depends': ['base'],
-    # # 'data': [
-    # #     'views/movie_view.xml',
-    # # ]
+    'author': "Mor Mbathie",
+    'website': "www.odootest.com",
+    'summary': "odoo 16 Development",
+    'category': "Tools",
+    'depends': ['base'],
+    'data': [
+        'views/movie_view.xml',  # Assurez-vous que le nom du fichier est correct
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
